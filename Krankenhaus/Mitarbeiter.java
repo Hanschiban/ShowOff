a84610e5c4bbc0aa236ac393a6704bc41e3f57b9
Hanschiban <Hannahkoss1990@gmail.com>
2016-10-20 00:17:44 +0200

    Update Mitarbeiter.java

--- Krankenhaus/Mitarbeiter.java
+++ Krankenhaus/Mitarbeiter.java
@@ -1,20 +1,19 @@
-
+//Von Person erbende Klasse mit den erweiterten Attributen Personalnummer und Gehalt
+//und einer Methode zum vergleichen von Gehältern
 public class Mitarbeiter extends Person
 {
-    public int personalnummer;
-    public int gehalt;
+  public int personalnummer;
+  public int gehalt;
     
 	public Mitarbeiter (String nachname, String vorname, boolean geschlecht, int personalnummer, int gehalt) 
 	{
 		super(nachname, vorname, geschlecht);
 		this.personalnummer = personalnummer;
-		this.gehalt = gehalt;
-			
+		this.gehalt = gehalt;	
 	}
 	
 	public boolean verdientMehrAls (Mitarbeiter x) 
 	{
 		return gehalt > x.gehalt;
-		
 	}
 }
