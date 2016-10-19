b924ad9ee09fce86bbe8b25e95128f57a8f03a6f
Hanschiban <Hannahkoss1990@gmail.com>
2016-10-20 00:20:42 +0200

    Update Krankenpfleger.java

--- Krankenhaus/Krankenpfleger.java
+++ Krankenhaus/Krankenpfleger.java
@@ -1,21 +1,17 @@
-
+//Von Mitarbeiter erbende Klasse nur mit Veränderter Anrede
 public class Krankenpfleger extends Mitarbeiter
 {
 	
 	public Krankenpfleger(String nachname, String vorname, boolean geschlecht, int personalnummer, int gehalt) 
 	{
 		super(nachname, vorname, geschlecht, personalnummer, gehalt);
-
 	}
 
 	
 	public String anrede ()
 	{
-		
 		if (geschlecht) return "Krankenschwester " + vorname + " " + nachname;
 		else return "Krankenpfleger " + vorname + " " + nachname;
-
-
 	}
 	
 	public String formloseAnrede () 
