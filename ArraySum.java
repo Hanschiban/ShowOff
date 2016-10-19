690de636d4ef91d59cfb53aa608990f382e3ff20
Hanschiban <Hannahkoss1990@gmail.com>
2016-10-19 17:18:47 +0200

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,16 @@
+//Gibt Summe der Werte eines int-Arrays zurück
+
+public static int summe(int x[])
+{
+	//Initialisierung der Summenvariable
+	int sum = 0;
+
+	//Iterierung über das Array zur einzelnen Addition der Werte in x[]
+	for(int i = 0 ; i < x.length ; i++)
+	{
+		sum = sum + x[i];
+	}
+	
+	//Rückgabe des Ergebnisses
+	return  sum;
+}
