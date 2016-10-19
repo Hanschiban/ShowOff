d90fb1573f11f305df7ca02ebc33749e38a9a6b9
Hanschiban <Hannahkoss1990@gmail.com>
2016-10-20 00:15:24 +0200

    Update Person.java

--- Krankenhaus/Person.java
+++ Krankenhaus/Person.java
@@ -1,24 +1,27 @@
+//Simple Standardklasse einer Person zur weiteren Vererbung
 public class Person 
 {
 	String nachname;
 	String vorname;
 	boolean geschlecht;
-
-
-    
+ 
 	public Person (String nachname, String vorname, boolean geschlecht) 
 	{
 		this.nachname = nachname;
 		this.vorname = vorname;
 		this.geschlecht = geschlecht;
 	}
-
  
-    
+  //Gibt Anrede basierend auf dem Geschlecht zurück
 	public String anrede () 
 	{
-		if (geschlecht) return "Frau" + vorname + " " + nachname;
-		else return "Herr" + vorname + " " + nachname;
-
+		if (geschlecht)
+    {
+      return "Frau" + vorname + " " + nachname;
+    }
+		else 
+    {
+      return "Herr" + vorname + " " + nachname;
+    }
 	}
 }
